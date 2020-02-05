94d9c52aec7999fdcab2907fc92c3f475b1c93d3
Long Tran <longthb3112@gmail.com>
2020-02-05 17:37:22 -0600

    Add Files

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebAPI 2.0 Practice
+
+* Build APIs based on Resources
+* Use and understand Status Codes
+* Design APIs with association URIs
+* Version your APIs
+
---
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile: Profile
+    {
+        public CampMappingProfile()
+        {
+            
+            CreateMap<Camp, CampModel>().ReverseMap();
+            CreateMap<Talk, TalkModel>().ReverseMap();
+            CreateMap<Speaker, SpeakerModel>().ReverseMap();
+        }
+    }
+}
